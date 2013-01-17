79b1165f9c50c9374b1f5123e65e3a44aeeb9012
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2013-01-17 15:26:23 +0100

    Initial commit, still have to refactor Log to Monolog, and make sure configuration is now passed via an array.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 cache
 =====
 
-A repository for caching written in PHP.
+This repository holds a wrapper around a caching system. You can use NoCache if no caching system is installed, or MemCache. This allows the user to provide caching in his code, and switch to other 
+caching systems later on if necessary.
+
+# Usage
+
+```php
+
+$c = Cache::getInstance( array("system" => "Memcache", "host" => "localhost", "port" => 11211 ) );
+$c->set("key", $objectToCache, $TTL); // TTL is optional
+
+$cachedObject = $c->get("key");
+// delete the cachedObject
+$c->delete("key");
+
+```
---
+++ src/tdt/cache/Cache.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The caching class. Depending on the globals given in the config it will be able to set and get a variable
+ *
+ * Usage of an implementation:
+ *   $c = Cache::getInstance();
+ *   $element = $c->get($id);
+ *   if(is_null($element)){
+ *      $element = get_the_right_data();
+ *      $c->set($id,$element,$timeout);
+ *   }
+ *
+ * @package tdt\cache
+ * @copyright (C) 2011,2013 by iRail vzw/asbl, OKFN Belgium vzw/asbl
+ * @license AGPLv3
+ * @author Jan Vansteenlandt <jan@iRail.be>
+ * @author Pieter Colpaert   <pieter@iRail.be>
+ */
+
+namespace tdt\cache;
+
+abstract class Cache{
+    private static $instance;
+    protected $config = array();
+    
+    
+    protected function __construct(){
+    }
+
+    /*
+     * The config expects:
+     * system = name of the caching system i.e. MemCache, NoCache,...
+     * host = the host on which the caching system runs
+     * port = the port on which to connect to the host
+     * 
+     * If NoCache is used, no host or port is necessary.
+     */
+    
+    public static function getInstance(array $config){
+        if(!isset(self::$instance)){
+            $this->config = $config;
+            if(isset($this->config["system"])){
+                $cacheclass = 'tdt\\cache\\' . $this->config["system"];
+                if(class_exists($cacheclass)){
+                    self::$instance = new $cacheclass();
+                }else{
+                    throw new TDTException(551,array("tdt\cache\ ".$this->config["system"]));
+                }
+            }else{
+                throw new TDTException(500,array("The system was not set in the configuration"));
+            }            
+            
+        }
+        return self::$instance;
+    }  
+    
+    abstract public function set($key, $value, $TTL = 60);
+    abstract public function get($key);
+    abstract public function delete($key);
+
+}
